4b9ffffe6ff9a3b07799038104c703c5624c946b
Beexiong <beexiong123@yahoo.com>
2017-01-25 17:50:09 -0600

    Traits and Name search complete 1.25.17

--- gui.js
+++ gui.js
@@ -1,30 +1,154 @@
+//search for multiple categories by associatig items to objects
 function initSearch(people){
-	;
+	var input = prompt("Do you know the name of the person you are searching for?");
+		if(input == "yes")
+		{
+			var NameSearchResults = initSearchByName(people);
+			displayResults(NameSearchResults);
+		}
+		else if(input == "no")
+		{
+			var traitSearchResults = initSearchByTraits(people);
+			displayResults(traitSearchResults);
+		}
+		else
+		{
+		alert("Invalid Entry. Please try again.");
+		initSearch(people);
+		}
 }
-function initSearchByName(){
-	;
+function initSearchByName(people){
+	var firstName = promptForFirstName();
+	var lastName = promptForLastName();
+	return people.filter(function(person){
+		if(firstName.toLowerCase() == person.firstName.toLowerCase() && lastName.toLowerCase() == person.lastName.toLowerCase())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	});
 }
-function initSearchByTraits(){
-	;
+function promptForFirstName(){
+	var firstName = prompt("Please type the First Name of the person.");
+	return firstName;
 }
-function displayResults(){
-	;
+function promptForLastName(){
+	var lastName = prompt("Please type the Last Name of the person");
+	return lastName;
 }
-function isNumeric() {
-	;
+function initSearchByTraits(people){
+	var userSelection = promptForTraits();
+	var trait = identifyTraitSelection(userSelection);
+	var cvtTrait = Number(trait);
+	if(userSelection == "age"){
+			var peopleCvtAge = convertPersonAge(people);
+			return peopleCvtAge.filter(function(person){
+			return (cvtTrait == person.dob);
+	});
+	}
+	else {
+		//var lowerCaseSelection = userSelection.toLowerCase();
+			return people.filter(function(person){
+			//if(trait.toLowerCase() == person[lowerCaseSelection] && trait.toLowerCase() == person[lowerCaseSelection])
+			return (trait.toLowerCase() == person.height || trait.toLowerCase() == person.weight ||trait.toLowerCase() == person.eyeColor ||trait.toLowerCase() == person.occupation);
+		});
+	}
 }
-function getAge(){
-	;
+function convertPersonAge(people){
+	var currentYear = getCurrentYear();
+	return people.map(function(person){
+		var birthday = person.dob;
+		var birthdayYear = getBirthdayYear(birthday);
+		person.dob = currentYear - birthdayYear;
+		return person;
+	});
 }
-function getHeight(){
-	;
+function getCurrentYear(){
+	var establishDate = new Date;
+	return establishDate.getFullYear();
 }
-function getWeight(){
-	;
+function getBirthdayYear(personBirthday){
+	var index = personBirthday.lastIndexOf("/");
+	var stringBirthdayYear = personBirthday.substr(index+1);
+	return Number(stringBirthdayYear);
 }
-function getEye(){
-	;
+function identifyTraitSelection(traitToSearch){
+	// while()
+	// {
+	switch (traitToSearch){
+		case "age":{
+			return promptForAge();
+		}
+		case "height":{
+			return promptForHeight();
+		}
+		case "weight":{
+			return promptForWeight();
+		}
+		case "eye color":{
+			return promptForEyeColor();
+		}
+		case "job":{
+			return promptForJob();
+		}
+		default:
+		("Invalid Selection. Please try again");
+		initSearchByTraits(traitToSearch);
+		}
 }
-function getOccupation(){
-	;
+function promptForTraits(){
+	return prompt("Please select the Trait to search by\nAge\nHeight\nWeight\nEye Color\nJob ");
 }
+function promptForAge(){
+	return prompt("Please select the age to search for");
+}
+function promptForHeight(){
+	return prompt("Please select the height to search for");
+}
+function promptForWeight(){
+	return prompt("Please select the weight to search for");
+}
+function promptForEyeColor(){
+	return prompt("Please select the eye color to search for");
+}
+function promptForJob(){
+	return prompt("Please select the occupation to search for");
+}
+function displayResults(results){
+	var arrayLength = results.length;
+	for (let i = 0; i < arrayLength;i++)
+	{
+		var object = results[0];
+		var firstName = object.firstName;
+		var lastName = object.lastName;
+		var age = object.dob;
+		var height = object.height;
+		var weight = object.weight;
+		var eyeColor = object.eyeColor;
+		var job = object.occupation;
+		alert('Here are the results for your entry: \nFirst Name: ' + firstName + '\nLastName: ' + lastName + '\nAge: ' + age + '\nWeight: ' + weight + '\nEye Color: ' + eyeColor + '\nOccupation: ' + job );
+	}
+
+}
+
+// function isNumeric() {
+// 	;
+// }
+// function getAge(){
+// 	;
+// }
+// function getHeight(){
+// 	;
+// }
+// function getWeight(){
+// 	;
+// }
+// function getEye(){
+// 	;
+// }
+// function getOccupation(){
+// 	;
+// }
